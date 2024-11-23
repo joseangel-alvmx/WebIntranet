19742462f0d0eb4369319168e3cb3ff3aab667a9
joseangel-alvmx <joseangel.mejia@alvamex.com.mx>
2024-11-23 12:35:47 -0600

    ajustes para produccion

--- app/Dashboard/models.py
+++ app/Dashboard/models.py
@@ -256,3 +256,26 @@ class Procesos(models.Model):
     tipo_documento = models.ForeignKey(TipoDocumento, on_delete=models.CASCADE)
     def __str__(self):
         return self.titulo
+    
+class Promociones(models.Model):
+    nombre = models.CharField((""), max_length=20)
+    imagen = models.FileField(upload_to='Promociones/')
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits = 10,decimal_places = 2)
+    cantidad = models.IntegerField(default=0)
+    empresa = models.ForeignKey(Empresas, on_delete=models.CASCADE, default=1)
+    inactiva = models.BooleanField(default=False)
+    fecha_promo = models.DateField(null=True)
+    def __str__(self):
+        return self.nombre
+    
+class BoletinMensual(models.Model):
+        mes_boletin = models.CharField(max_length=50)
+        empresa = models.ForeignKey(Empresas, on_delete=models.CASCADE, default=1)
+        boletin = models.FileField(upload_to='Boletins/')
+        fecha_carga = models.DateTimeField(auto_now_add=True)
+        fecha_boletin= models.DateField(null=True)
+        nombre_planta = models.CharField(max_length=50)
+        def __str__(self):
+            return self.mes_boletin + '-' +self.nombre_planta
+        
